8c8f87ae6d03353299167867115130b61d9da96c
andres-condezo <ra.condezomonge@gmail.com>
2021-08-13 21:45:46 -0500

    Style(0-create_a_file.pp): [Add properly indentation to create_a_file manifest]

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # create a file in /tmp
-file { '/tmp/holberton'
+file { '/tmp/holberton':
   ensure  => present,
-  path  => '/tmp/holberton',
-  mode  => '0744',
-  owner  => 'www-data',
-  group  => 'www-data',
-  content  => 'I love Puppet',
+  path    => '/tmp/holberton',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
