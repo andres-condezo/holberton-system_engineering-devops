6d9465bf25beaaeffed32efaa6e62e22d6986584
andres-condezo <ra.condezomonge@gmail.com>
2021-08-15 23:08:58 -0500

    Feat(100-puppet_ssh_config.pp): [Add puppet_ssh_config script]

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,13 @@
 # Connect to a server without typing a password and use the private key ~/.ssh/holberton
 file_line { 'Turn off passwd auth':
-  ensure            => created,
+  ensure            => 'present',
   path              => '/etc/ssh/ssh_config',
   line              => 'PasswordAuthentication no',
   replace           => true,
 }
 
 file_line { 'Declare identity file':
-  ensure            => created,
+  ensure            => 'present',
   path              => '/etc/ssh/ssh_config',
   line              => 'IdentityFile ~/.ssh/holberton',
   replace           => true,
