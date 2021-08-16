fee6117f05a8eff3d8ba9362f04c72ec50f6260c
andres-condezo <ra.condezomonge@gmail.com>
2021-08-15 23:13:48 -0500

    Feat(100-puppet_ssh_config.pp): [Add puppet_ssh_config script]

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -3,6 +3,7 @@ file_line { 'Turn off passwd auth':
   ensure            => 'present',
   path              => '/etc/ssh/ssh_config',
   line              => 'PasswordAuthentication no',
+  match             => 'PasswordAuthentication yes',
   replace           => true,
 }
 
@@ -10,5 +11,6 @@ file_line { 'Declare identity file':
   ensure            => 'present',
   path              => '/etc/ssh/ssh_config',
   line              => 'IdentityFile ~/.ssh/holberton',
+  match             => 'IdentityFile ~/.ssh/if_rsa',
   replace           => true,
 }
