9ab867d5df4975ebfc81075c475631ba2dd15c8f
andres-condezo <ra.condezomonge@gmail.com>
2021-09-28 19:56:11 -0500

    Feat(0-subs.py): [create subs script]

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Module for number_of_subscribers function
+"""
+import requests
+
+def number_of_subscribers(subreddit):
+    """
+    A function that queries the Reddit API and returns the number of subscribers for a given subreddit.
+    """
+    res = requests.get("http://reddit.com/r/{}/about.json".format(subreddit), headers={'User-Agent': 'Custom'})
+
+    if res.status_code == 200:
+        return res.json.get('data').get('subscribers')
+    else:
+        return 0
