31c476e542354db0ffbda6e5a2f2cbab57a031ac
andres-condezo <ra.condezomonge@gmail.com>
2021-09-28 22:39:58 -0500

    Feat(1-top_ten.py): [Fix pep8 warnings]

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -4,16 +4,22 @@ Module for top_ten function
 """
 import requests
 
+
 def top_ten(subreddit):
     """
-    A function that queries the Reddit API and prints the titles of the first 10 hot posts
+    A function that queries the Reddit API
+    and prints the titles of the first 10 hot posts
     """
-    res = requests.get("https://www.reddit.com/r/{}/hot/.json".format(subreddit), headers={'User-Agent': 'Custom'}, params={'limit': 10}, allow_redirects=False)
+    res = requests.get("https://www.reddit.com/r/{}/hot/.json"
+                       .format(subreddit),
+                       headers={'User-Agent': 'Custom'},
+                       params={'limit': 10},
+                       allow_redirects=False)
 
     response = res.json().get('data', None)
 
     if response is None:
-        print(None)
+        print('None')
         return
 
     response = response.get('children', [])
