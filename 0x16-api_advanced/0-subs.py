cae60256f1ec3d2725a7e8ed805e2c1ab121f1bc
andres-condezo <ra.condezomonge@gmail.com>
2021-09-28 22:58:04 -0500

    Feat(0-subs.py): [refactor the code]

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,13 +4,18 @@ Module for number_of_subscribers function
 """
 import requests
 
+
 def number_of_subscribers(subreddit):
     """
-    A function that queries the Reddit API and returns the number of subscribers for a given subreddit.
+    A function that queries the Reddit API
+    and returns the number of subscribers for a given subreddit.
     """
-    res = requests.get("http://reddit.com/r/{}/about.json".format(subreddit), headers={'User-Agent': 'Custom'}, allow_redirects=False)
+    res = requests.get("https://www.reddit.com/r/{}/about.json"
+                       .format(subreddit),
+                       headers={'User-Agent': 'Custom'},
+                       allow_redirects=False)
 
-    if res.status_code == 200:
-        return res.json().get('data').get('subscribers')
-    else:
+    if res is None:
         return 0
+
+    return res.json().get('data', {}).get('subscribers', 0)
