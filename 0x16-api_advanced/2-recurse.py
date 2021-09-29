5fd16900d64134f7e53a9eca14f78e097810ac27
andres-condezo <ra.condezomonge@gmail.com>
2021-09-29 00:03:06 -0500

    Feat(): [refactor the code]

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -5,7 +5,7 @@ Module for recurse function
 import requests
 
 
-def recurse(subreddit, hot_list=[], ):
+def recurse(subreddit, hot_list=[], new_after):
     """
 A recursive function that queries the Reddit API
 and returns a list containing the titles of all hot articles
