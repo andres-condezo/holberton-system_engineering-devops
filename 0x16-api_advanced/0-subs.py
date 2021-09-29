c702ebd0b8e140d9e20227e92d7ca4844bffc1c9
andres-condezo <ra.condezomonge@gmail.com>
2021-09-28 20:18:22 -0500

    Feat(0-subs.py): [add redirects restriction]

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,9 +8,9 @@ def number_of_subscribers(subreddit):
     """
     A function that queries the Reddit API and returns the number of subscribers for a given subreddit.
     """
-    res = requests.get("http://reddit.com/r/{}/about.json".format(subreddit), headers={'User-Agent': 'Custom'})
+    res = requests.get("http://reddit.com/r/{}/about.json".format(subreddit), headers={'User-Agent': 'Custom'}, allow_redirects=False)
 
     if res.status_code == 200:
-        return res.json.get('data').get('subscribers')
+        return res.json().get('data').get('subscribers')
     else:
         return 0
