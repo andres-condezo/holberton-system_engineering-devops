09156c28766a1d03dcfe7615b556e50c2a4d4d17
andres-condezo <ra.condezomonge@gmail.com>
2021-09-28 23:59:20 -0500

    Feat(0-subs.py): [refactor the code]

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Module for recurse function
+"""
+import requests
+
+
+def recurse(subreddit, hot_list=[], ):
+    """
+A recursive function that queries the Reddit API and returns a list containing the titles of all hot articles
+    """
+
+    response = requests.get("https://www.reddit.com/r/{}/hot.json"
+                       .format(subreddit),
+                       headers={'User-Agent': 'Custom'},
+                       params={'after': 'new_after'},
+                       allow_redirects=False)
+
+    response = response.json().get('data', None)
+
+    if response is None:
+        return None
+
+    response = response.get('children', [])
+
+    for post in response:
+        hot_list.append(post.get('data', {}).get('title', ''))
+
+    new_after = response.get('data', {}).get('after', '')
+
+    if new_after is None:
+        return hot_list
+
+    return recurse(subreddit, hot_list, new_after)
