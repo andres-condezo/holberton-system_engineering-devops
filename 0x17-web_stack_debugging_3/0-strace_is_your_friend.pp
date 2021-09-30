e812faaec1bd6053ce2d49851281960007c27d48
andres-condezo <ra.condezomonge@gmail.com>
2021-09-29 19:35:51 -0500

    Feat(0-strace_is_your_friend.pp): [add a comment to puppet script]

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,3 +1,4 @@
+# fix the phpp typo
 exec { 'fix-wordpress':
   command => 'sudo sed -i s/class-wp-locale.phpp/class-wp-locale.php/g /var/www/html/wp-settings.php',
   path    => ['/usr/bin', '/usr/sbin',],
